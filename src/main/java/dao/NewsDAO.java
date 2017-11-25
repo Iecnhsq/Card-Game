f07d67ce9d14332a93f8d312c76f7e21e2c6a97f
Iecnhsq <31551720+Iecnhsq@users.noreply.github.com>
2017-11-26 00:34:01 +0200

    Add files via upload

---
+++ src/main/java/dao/NewsDAO.java
@@ -0,0 +1,58 @@
+package dao;
+
+import entity.News;
+import hibernate.HibernateUtil;
+import java.util.List;
+import org.hibernate.HibernateException;
+import org.hibernate.Query;
+import org.hibernate.Session;
+
+public class NewsDAO {
+
+    public List<News> getNews() {
+        List<News> out;
+        Session s = HibernateUtil.getSESSIONFACTORY().openSession();
+        out = s.createQuery("FROM News").list();
+        s.close();
+        return out;
+    }
+
+    public News getNewsById(int id) {
+        Session s = HibernateUtil.getSESSIONFACTORY().openSession();
+        News out = null;
+        s.beginTransaction();
+        out = (News) s.createQuery("FROM News WHERE id='" + id + "'").uniqueResult();
+        s.getTransaction().commit();
+        s.close();
+        return out;
+    }
+
+    public void addNews(News n) {
+        Session s = HibernateUtil.getSESSIONFACTORY().openSession();
+        s.beginTransaction();
+        s.save(n);
+        s.getTransaction().commit();
+        s.close();
+    }
+
+    public int getMaxId() {
+        int maxId = 0;
+        try {
+            Session s = HibernateUtil.getSESSIONFACTORY().openSession();
+            s.beginTransaction();
+            Query q = s.createQuery("Select max(id) From News");
+            List currentSeq = q.list();
+            if (currentSeq.contains(null)) {
+                maxId = 1;
+                return maxId;
+            } else {
+                maxId = (int) currentSeq.get(0);
+                return maxId + 1;
+            }
+        } catch (HibernateException exc) {
+            System.out.print("Unable to get latestID");
+        }
+        return maxId;
+    }
+
+}
---
+++ src/main/java/dao/UserDAO.java
@@ -0,0 +1,60 @@
+package dao;
+
+import entity.User;
+import hibernate.HibernateUtil;
+import java.util.List;
+import org.hibernate.Session;
+
+public class UserDAO {
+
+    List<User> getUser() {
+        List<User> out;
+        Session s = HibernateUtil.getSESSIONFACTORY().openSession();
+        out = s.createQuery("FROM User").list();
+        s.close();
+        return out;
+    }
+
+    public User getUserByLogin(String login) {
+        Session s = HibernateUtil.getSESSIONFACTORY().openSession();
+        User out = null;
+        s.beginTransaction();
+        out = (User) s.createQuery("FROM User WHERE login='" + login + "'").uniqueResult();
+        s.getTransaction().commit();
+        s.close();
+        return out;
+    }
+
+    public void addUser(User u) {
+        Session s = HibernateUtil.getSESSIONFACTORY().openSession();
+        s.beginTransaction();
+        s.save(u);
+        s.getTransaction().commit();
+        s.close();
+    }
+
+    public void updateUser(User u) {
+        Session s = HibernateUtil.getSESSIONFACTORY().openSession();
+        s.beginTransaction();
+        s.update(u);
+        s.getTransaction().commit();
+        s.close();
+    }
+
+    public User getUserMail(String email) { //// пусть будет, пригодится
+        Session s = HibernateUtil.getSESSIONFACTORY().openSession();
+        User out = null;
+        s.beginTransaction();
+        out = (User) s.createQuery("FROM User WHERE email='" + email + "'").uniqueResult();
+        s.getTransaction().commit();
+        s.close();
+        return null;
+    }
+
+    public boolean isExists(String login) {
+        Session s = HibernateUtil.getSESSIONFACTORY().openSession();
+        User u = (User) s.createQuery("FROM User WHERE login='" + login + "'").uniqueResult();
+        return u != null;
+    }
+
+}
