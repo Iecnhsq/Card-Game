356f45015ecaaa9c391cef6eb4a83e554f709aab
Iecnhsq <31551720+Iecnhsq@users.noreply.github.com>
2017-11-26 00:34:59 +0200

    Add files via upload

---
+++ src/main/java/hibernate/HibernateUtil.java
@@ -0,0 +1,24 @@
+package hibernate;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
+
+public class HibernateUtil {
+
+    private static final SessionFactory SESSIONFACTORY = buildSessionFactory();
+    private static ServiceRegistry serviceRegistry;
+
+    private static SessionFactory buildSessionFactory() {
+
+        Configuration configuration = new Configuration();
+        configuration.configure();
+        serviceRegistry = new StandardServiceRegistryBuilder().applySettings(configuration.getProperties()).build();
+        return configuration.buildSessionFactory(serviceRegistry);
+    }
+
+    public static SessionFactory getSESSIONFACTORY() {
+        return SESSIONFACTORY;
+    }
+}
